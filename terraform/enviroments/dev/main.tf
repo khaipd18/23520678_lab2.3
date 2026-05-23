3765bc080cdb4cc99a11d980c11562218d005aaf
Phan Đình Khải <174919444+khaipd18@users.noreply.github.com>
2026-05-23 20:20:57 +0700

    Add Terraform modules for VPC, EKS, and ECR configuration

--- terraform/enviroments/dev/main.tf
+++ terraform/enviroments/dev/main.tf
@@ -1,1 +1,17 @@
 
+module "vpc" {
+  source       = "../../modules/vpc"
+  cluster_name = var.cluster_name
+}
+
+module "eks" {
+  source       = "../../modules/eks"
+  cluster_name = var.cluster_name
+  vpc_id       = module.vpc.vpc_id
+  subnet_ids   = module.vpc.private_subnets
+}
+
+module "ecr" {
+  source       = "../../modules/ecr_oidc"
+  repositories = var.ecr_repos
+}
