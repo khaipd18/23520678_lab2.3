3fd563b18aedcff44cb75929159467a7c6f52f6e
Phan Đình Khải <174919444+khaipd18@users.noreply.github.com>
2026-05-23 22:59:29 +0700

    .

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -24,6 +24,9 @@ module "eks" {
     }
   }
 
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
   # Cấp quyền admin cho người tạo cluster
   enable_cluster_creator_admin_permissions = true
 
