123b5fca613bbf4af7d05f967d396e841e3ae501
Phan Đình Khải <174919444+khaipd18@users.noreply.github.com>
2026-05-23 21:46:53 +0700

    .

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  cluster_version = "1.35"
 
   vpc_id                   = var.vpc_id
   subnet_ids               = var.subnet_ids
